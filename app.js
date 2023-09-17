d289c74adc9cd42f2780c3e41ea5f572aa157c57
John Lee <john.lee@ufl.edu>
2023-09-17 15:32:24 -0400

    update2

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
+const { default: axios } = require('axios')
 const express = require('express')
 const { getCityInfo, getJobs } = require('./util')
 const app = express()
 
-app.use(express.json)
+
 // // TODO: import the getCityInfo and getJobs functions from util.js
-app.use('/api', { getCityInfo, getJobs })
+
 // TODO: Statically serve the public folder
 app.use(express.static('public'))
 // TODO: declare the GET route /api/city/:city
@@ -16,15 +17,16 @@ app.use(express.static('public'))
 // jobs (with value of the getJobs function)
 // If no city info or jobs are found,
 // the endpoint should return a 404 status
-app.get('/api/city/:city', (req,res) => {
-    const cityInfo = getCityInfo[req.params.index]
-    const jobs = getJobs[req.params.index]
-    if(!cityInfo)
-        return res.status(404).send('Location Not Found')
-    if(!jobs)
-        return res.status(404).send('Data Not Found')
-    res.json(cityInfo, jobs)
-}
-)
+app.get('/api/city/:city', async (req,res) => {
+    try{
+        const cityInfo = await getCityInfo(req.params.city)
+        const jobs = await getJobs(req.params.city)
+        res.json({cityInfo, jobs})
+    }catch(err){
+        res.status(404).send('Data Not Found')
+    }
+    }
+    )
+    
 
 module.exports = app
